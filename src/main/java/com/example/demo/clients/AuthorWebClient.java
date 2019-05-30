7df96079c3c4b7cbcefe91b5c739f7026d29943e
Shireesha Bongarala <bongaralashireesha@gmail.com>
2019-05-30 16:16:12 +0530

    Add missing service annotation

--- src/main/java/com/example/demo/clients/AuthorWebClient.java
+++ src/main/java/com/example/demo/clients/AuthorWebClient.java
@@ -2,9 +2,11 @@ package com.example.demo.clients;
 
 import com.example.demo.domain.Author;
 import org.springframework.beans.factory.annotation.Value;
+import org.springframework.stereotype.Service;
 import org.springframework.web.reactive.function.client.WebClient;
 import reactor.core.publisher.Mono;
 
+@Service
 public class AuthorWebClient {
 
   private final WebClient webClient;
